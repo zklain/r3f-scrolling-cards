7e8810c6384a5203c697f640c807e960749a10f2
Zdenko Klain <zdnkk@protonmail.com>
2023-07-02 14:16:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## R3F Starter Repo
+
+### Deps
+
+- vite
+- React@18 w. TS
+- @react-three/fiber
+- @react-three/drei
+- three
+- leva
+
+### TODO
+
+- [] eslint
+- [] router
+- [] husky
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Box, Environment, OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <OrbitControls makeDefault />
+        <ambientLight />
+        <color attach="background" args={['#2d1b50']} />
+        <Box args={[1, 1, 1]}>
+          <meshStandardMaterial color="white" />
+        </Box>
+        <Environment preset="forest" />
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
